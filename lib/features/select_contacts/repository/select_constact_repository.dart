73e2427f114d7c4a9a5656a24019b48fde326e08
BINN\binn <thanhbinhdev08@gmail.com>
2024-05-04 23:22:13 +0700

    + add permission read and write contact

--- lib/features/select_contacts/repository/select_constact_repository.dart
+++ lib/features/select_contacts/repository/select_constact_repository.dart
@@ -1,1 +1,9 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+class SelectContactRepository {
+  final FirebaseFirestore firestore;
+  SelectContactRepository({
+    required this.firestore,
+  });
+}
